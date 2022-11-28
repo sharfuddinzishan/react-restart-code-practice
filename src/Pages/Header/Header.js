5454032156db0219341e2647a4abc08373a95514
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2022-11-28 20:45:20 +0600

    Pages

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+const Header = () => {
+    return (
+        <div>
+            <h2>Header</h2>
+            <Link to="/infos">All Infos</Link>
+            <Link to="/add/info">Add Info</Link>
+        </div>
+    );
+};
+
+export default Header;<h2>Header</h2>
